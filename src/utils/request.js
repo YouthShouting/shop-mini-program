ba8a8de3afccc92e6520d3d5242e451b714580cc
YouthShouting <1972199551@qq.com>
2019-02-24 18:20:49 +0800

    修改request.baseURL

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,4 @@
+    // 主要是对 wx.request 方法进行封装
     function request(url,method="GET",data={}){
         return new Promise((resolve,reject)=>{
             wx.request({
@@ -10,8 +11,11 @@
             })
         })
     }
-
-    request.baseURL = "https://www.zhengzhicheng.cn/api/public/v1/"
+    // 有证书，但是经常容易挂
+    // request.baseURL = "https://www.zhengzhicheng.cn/api/public/v1/"
+    // 证书过期，但是取消域名校验，还可以使用
+    // request.baseURL = "https://itjustfun.cn/api/public/v1/"
+    request.baseURL = "https://autumnfish.cn/wx/api/public/v1/";
     request.get = function (url, data){
         return request(request.baseURL + url, "GET", data)
     }
